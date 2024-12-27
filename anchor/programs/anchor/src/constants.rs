62caac200461b24e3090263121f7cf8557681329
mrb1nary <anubhab.techie@gmail.com>
2024-12-27 15:17:10 +0530

    Added important functionalities to utils file

--- anchor/programs/anchor/src/constants.rs
+++ anchor/programs/anchor/src/constants.rs
@@ -7,9 +7,9 @@ pub const SEED_SOL_ACC: &[u8] = b"sol_acc";
 
 #[constant]
 pub const MINT_DECIMALS: u8 = 9;
-pub const LIQUIDATION_BONUS: u8 = 10;
-pub const MIN_HEALTH_FACTOR: u8 = 1;
-pub const LIQUIDATION_THRESHOLD: u8 = 50;
+pub const LIQUIDATION_BONUS: u64 = 10;
+pub const MIN_HEALTH_FACTOR: u64 = 1;
+pub const LIQUIDATION_THRESHOLD: u64 = 50;
 
 pub const FEED_ID: &str = "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d";
 pub const MAXIMUM_AGE_PRICE_FEED: u64 = 60;
--- anchor/programs/anchor/src/instructions/utils.rs
+++ anchor/programs/anchor/src/instructions/utils.rs
@@ -1,7 +1,8 @@
 use std::u64::MAX;
 
 use anchor_lang::prelude::*;
-use pyth_solana_receiver_sdk::price_update::{self, get_feed_id_from_hex, PriceUpdateV2};
+use anchor_lang::solana_program::native_token::LAMPORTS_PER_SOL;
+use pyth_solana_receiver_sdk::price_update::{self, get_feed_id_from_hex, Price, PriceUpdateV2};
 
 use crate::error::CustomError;
 use crate::{
@@ -45,7 +46,7 @@ pub fn calculate_health_factor(
 
     let health_factor =
         (collateral_adjusted_after_liquidation_threshold) / collateralAcc.amount_minted;
-    Ok(())
+    Ok(health_factor)
 }
 
 fn get_usd_value(amount_in_lamports: &u64, price_feed: &Account<PriceUpdateV2>) -> Result<u64> {
@@ -56,4 +57,39 @@ fn get_usd_value(amount_in_lamports: &u64, price_feed: &Account<PriceUpdateV2>) 
     require!(price.price > 0, CustomError::InvalidPrice);
 
     let price_in_usd = price.price as u128 * PRICE_FEED_DECIMAL_ADJUSTMENT;
+
+    let amount_in_usd = (*amount_in_lamports as u128 * price_in_usd) / (LAMPORTS_PER_SOL as u128);
+
+    msg!("*** CONVERT USD TO SOL ***");
+    msg!("SOL/USD Price : {:.9}", price_in_usd as f64 / 1e9);
+    msg!("SOL Amount    : {:.9}", *amount_in_lamports as f64 / 1e9);
+    msg!("USD Value     : {:.9}", amount_in_usd as f64 / 1e9);
+
+    Ok(amount_in_usd as u64)
+}
+
+pub fn get_lamports_from_usd(
+    amount_in_usd: &u64,
+    price_feed: &Account<PriceUpdateV2>,
+) -> Result<u64> {
+    let feed_id = get_feed_id_from_hex(FEED_ID)?;
+    let price =
+        price_feed.get_price_no_older_than(&Clock::get()?, MAXIMUM_AGE_PRICE_FEED, &feed_id)?;
+
+    // Check price is positive
+    require!(price.price > 0, CustomError::InvalidPrice);
+
+    // Adjust price to match lamports precision (9 decimals)
+    // Example: Assuming 1 SOL = $2.00
+    // price.price = 200_000_000 (from Pyth, 8 decimals)
+    // price_in_usd = 200_000_000 * 10 = 2_000_000_000 (9 decimals)
+    let price_in_usd = price.price as u128 * PRICE_FEED_DECIMAL_ADJUSTMENT;
+
+    let amount_in_lamports = ((*amount_in_usd as u128) * (LAMPORTS_PER_SOL as u128)) / price_in_usd;
+    msg!("*** CONVERT SOL TO USD ***");
+    msg!("SOL/USD Price : {:.9}", price_in_usd as f64 / 1e9);
+    msg!("USD Amount    : {:.9}", *amount_in_usd as f64 / 1e9);
+    msg!("SOL Value     : {:.9}", amount_in_lamports as f64 / 1e9);
+
+    Ok(amount_in_lamports as u64)
 }
